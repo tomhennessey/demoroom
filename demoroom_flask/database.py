c4344eb3beacb83b7f35e29f64b8ad652d7c22f6
Tom Hennessey <tomahennessey@gmail.com>
2020-06-26 12:14:20 -0500

    updated db creation for windows

--- demoroom_flask/database.py
+++ demoroom_flask/database.py
@@ -3,8 +3,8 @@
 # 'flask db migrate -m "demos table" ' 
 # 'flask db upgrade'
 # and then run this script to populate the database
-
-from os import listdir, walk
+from sys import platform as _platform
+from os import listdir, walk, name
 from os.path import isfile, join
 import itertools
 from app import db
@@ -16,24 +16,46 @@ categories = []
 texts = []
 proper_names = []
 
-for f in listdir(text_demos):
-    names.append(f.replace('.txt', ''))
 
-for f in listdir(text_demos):
-    file = open(text_demos + f, 'r')
-    currText = file.read().replace('\n', ' ')
-    texts.append(currText)
+def get_names():
+    for f in listdir(text_demos):
+        names.append(f.replace('.txt', ''))
 
-for f in listdir(text_demos):
-    file = open(text_demos + f, 'r')
-    currline = " ".join(file.readline().split())
-    proper_names.append(currline)
+# Will remove posix newlines ('\n') and replace w/ spaces
+def get_texts_posix():
+    for f in listdir(text_demos):
+        file = open(text_demos + f, 'r')
+        currText = file.read().replace('\n', ' ')
+        texts.append(currText)
 
-for (a, b, c) in zip(names, texts, proper_names):
-    d = Demo(name=a, text=b, proper_name=c)
-    db.session.add(d)
-    db.session.commit()
+# Windows uses '\r\n' as newline so we need a different version
+# of the above if that's what's running
+def get_texts_windows():
+    for f in listdir(text_demos):
+        file = open(text_demos + f, 'r')
+        currText = file.read().replace('\r\n', ' ')
+        texts.append(currText)
 
+def get_proper_names():
+    for f in listdir(text_demos):
+        file = open(text_demos + f, 'r')
+        currline = " ".join(file.readline().split())
+        proper_names.append(currline)
 
+def add_to_db():
+    for (a, b, c) in zip(names, texts, proper_names):
+        d = Demo(name=a, text=b, proper_name=c)
+        db.session.commit()
+
+get_names()
+if _platform == "linux" or _plaform == "linux2" or _platform == "darwin":
+    get_texts_posix()
+elif _platform == "win32" or _platform == "win64":
+    get_texts_windows()
+else:
+    print("Unable to get texts -- platform error")
+get_proper_names()
+add_to_db()
+print(_platform)
 
 
