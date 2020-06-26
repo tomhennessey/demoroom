2024e417351e0840a958ca2c60bb1f896153f919
Tom Hennessey <tomahennessey@gmail.com>
2020-06-26 16:22:27 -0500

    added category function

--- demoroom_flask/database.py
+++ demoroom_flask/database.py
@@ -16,10 +16,11 @@ categories = []
 texts = []
 proper_names = []
 
-
+# grab the no-spaced "ugly" name of a demo
 def get_names():
     for f in listdir(text_demos):
         names.append(f.replace('.txt', ''))
+    print("get_names done")
 
 # Will remove posix newlines ('\n') and replace w/ spaces
 def get_texts_posix():
@@ -27,6 +28,7 @@ def get_texts_posix():
         file = open(text_demos + f, 'r')
         currText = file.read().replace('\n', ' ')
         texts.append(currText)
+    print("get_texts done")
 
 # Windows uses '\r\n' as newline so we need a different version
 # of the above if that's what's running
@@ -35,27 +37,45 @@ def get_texts_windows():
         file = open(text_demos + f, 'r')
         currText = file.read().replace('\r\n', ' ')
         texts.append(currText)
+    print("get_texts done")
 
+# grab the "good-looking" name of a demo
 def get_proper_names():
     for f in listdir(text_demos):
         file = open(text_demos + f, 'r')
         currline = " ".join(file.readline().split())
         proper_names.append(currline)
+    print("get_proper_names done")
 
+def get_categories():
+    for f in listdir(text_demos):
+        file = open(text_demos + f, 'r')
+        firstline = file.readline()
+        # only want the last line of the file where category is
+        for lastline in file:
+            pass
+        # get rid of extra spaces
+        lastline = " ".join(lastline.split())
+        # remove page number
+        lastline = ''.join([i for i in lastline if not i.isdigit()])
+        # get rid of spaces around slashes
+        lastline = lastline.replace(' /', '/')
+        lastline = lastline.replace('/ ', '/')
+
+        # add to the list
+        categories.append(lastline)
+    print("get_categories done")
+
+
+# goes collects elements from all lists to form database entries and adds them
 def add_to_db():
     for (a, b, c) in zip(names, texts, proper_names):
-        d = Demo(name=a, text=b, proper_name=c)
+        d = Demo(name=a, text=b, proper_name=c) 
         db.session.commit()
+    print("committed to db")
 
 get_names()
-if _platform == "linux" or _plaform == "linux2" or _platform == "darwin":
-    get_texts_posix()
-elif _platform == "win32" or _platform == "win64":
-    get_texts_windows()
-else:
-    print("Unable to get texts -- platform error")
+get_texts_posix()
 get_proper_names()
+get_categories()
 add_to_db()
-print(_platform)
-
-
