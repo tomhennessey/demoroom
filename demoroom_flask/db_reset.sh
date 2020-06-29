0e0b6ca0d982bfecb42f6bd8896972bc0e0a00c4
Tom Hennessey <tomahennessey@gmail.com>
2020-06-29 09:39:10 -0500

    added db cleaner and builder script

---
+++ demoroom_flask/db_reset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$#" != 1 ]; then
+	echo "Run script with c to clean or i to init"
+	exit 0
+fi
+
+if [ $1 == "i" ]; then
+	flask db init
+	flask db migrate
+	flask db upgrade
+	exit 0
+
+elif [ $1 == "c" ]; then
+	DB=./app.db
+	MIG=./migrations/
+	if test -f "$DB"; then
+		rm $DB
+	fi
+	if test -d "$MIG"; then
+		rm -rf $MIG
+	fi
+	exit 0
+fi
