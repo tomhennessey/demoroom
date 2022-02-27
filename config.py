b02ae000dcb636f03594d0460895d113d110a894
Tom Hennessey <tomahennessey@gmail.com>
2022-02-27 16:20:21 -0600

    Removed old config

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'b6317sv5'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-            'postgres://', 'postgresql://') or \
-            'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
